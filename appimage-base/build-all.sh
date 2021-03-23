cf88ff91afc80cd9afe655117cc3ada998caf119
Joseph Lenox <lenox.joseph@gmail.com>
2021-03-23 10:48:39 -0500

    add submodule for dependencies

--- appimage-base/build-all.sh
+++ appimage-base/build-all.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 mkdir -p /build/slic3r
 cd /build/slic3r
-cmake -DCMAKE_PREFIX_PATH=${1}/usr/local -DSLIC3R_STATIC=1 /source
-cmake -j4 Slic3r
+cmake -DCMAKE_PREFIX_PATH=${1}/destdir/usr/local -DSLIC3R_STATIC=1 /source
+make -j4 Slic3r
